1314e20d252237868ee3046afb717879258dced2
Nguyễn Tùng Dương <nguyenduong12102004@gmail.com>
2026-03-20 15:52:47 +0700

    Cap nhap ma nguon bai 6

---
+++ src/main/java/com/example/nguyentungduong/NguyentungduongApplication.java
@@ -0,0 +1,13 @@
+package com.example.nguyentungduong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyentungduongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyentungduongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nguyentungduong/NguyentungduongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nguyentungduong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyentungduongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
